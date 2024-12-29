dab69324badaee0b6b13ccb089aab7f3d17bc0ac
Kazem Firouzmandi <kf1375@hotmail.com>
2024-12-30 00:37:40 +0330

    fix: update foreign key references from UserID to TelegramID in Devices and UserInteractions tables

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS Devices (
     DeviceID SERIAL PRIMARY KEY,
     DeviceName TEXT NOT NULL,
     Status TEXT DEFAULT 'Active',
-    UserID INTEGER NOT NULL,
+    TelegramID INTEGER NOT NULL,
     CreatedAt TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user
-        FOREIGN KEY (UserID)
-        REFERENCES Users(UserID)
+        FOREIGN KEY (TelegramID)
+        REFERENCES Users(TelegramID)
         ON DELETE CASCADE
 );
 
@@ -36,14 +36,14 @@ CREATE TABLE IF NOT EXISTS DeviceData (
 -- UserInteractions Table
 CREATE TABLE IF NOT EXISTS UserInteractions (
     InteractionID SERIAL PRIMARY KEY,
-    UserID INTEGER NOT NULL,
+    TelegramID INTEGER NOT NULL,
     DeviceID INTEGER NOT NULL,
     Action TEXT NOT NULL,
     Details TEXT,  -- Optionally, you can change this to JSON or JSONB for structured data
     Timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_user_interaction
-        FOREIGN KEY (UserID)
-        REFERENCES Users(UserID)
+        FOREIGN KEY (TelegramID)
+        REFERENCES Users(TelegramID)
         ON DELETE CASCADE,
     CONSTRAINT fk_device_interaction
         FOREIGN KEY (DeviceID)
