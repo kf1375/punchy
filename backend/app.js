6e973d82f512d1cb33a45d2c70bfa5f4cfe3cbf6
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-03 16:01:56 +0330

    fix: update check_user route path to remove API prefix

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const port = 8000;
 app.use(express.json());
 
 // Sample POST route for checking if the user exists
-app.post('/api/check_user', (req, res) => {
+app.post('/check_user', (req, res) => {
     const { userId } = req.body;
     console.log('Check User')
     if (userId === '12345') {
