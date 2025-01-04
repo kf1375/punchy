0acebcd8993001215fe859a2a9d1c2fbb8ac562b
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-04 21:44:13 +0330

    fix: refactor ProtectedRoute to use Navigate for redirection based on authentication state

--- frontend/src/Components/ProtectedRoute/ProtectedRoute.js
+++ frontend/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, userState }) => {
-  const navigate = useNavigate();
+  const { isAuthenticated } = userState || {};
+
   // Redirect to login if the user is not authenticated
-  return userState ? children : navigate('/login');
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
