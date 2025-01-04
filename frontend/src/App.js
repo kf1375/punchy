1914728b29ef81df8735f8330adb574135cb12b1
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-04 20:42:31 +0330

    fix: correct import statement for Routes in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routesm, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
