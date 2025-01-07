da2e7aa820b474a0dbf417e8a1b022ecdf4d34d0
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-07 22:57:51 +0330

    feat: remove python telegram bot implementation and related configuration files

--- telegram_bot/config/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-DATABASE_URL = os.getenv('DATABASE_URL')
-MQTT_BROKER = os.getenv("MQTT_BROKER", "mosquitto")
-MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
