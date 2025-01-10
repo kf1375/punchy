7169db45fea2ca0f1d8d6b0e76b411333b97cdd7
Kazem Firouzmandi <kf1375@hotmail.com>
2025-01-10 14:35:59 +0330

    refactor: add fullWidth property to Tabs component in DeviceControlPanel

--- frontend/src/pages/DeviceControlPanel.js
+++ frontend/src/pages/DeviceControlPanel.js
@@ -163,7 +163,7 @@ const DeviceControlPanel = () => {
 
     return (
         <Container>
-            <Tabs value={activeTab} onChange={handleTabChange} sx={{ width: '100%', justifyContent: 'space-evenly'}}>
+            <Tabs value={activeTab} onChange={handleTabChange} sx={{ width: '100%', justifyContent: 'space-evenly'}} fullWidth>
                 <Tab label="Manual" />
                 <Tab label="Automatic" />
                 <Tab label="Setting" />
